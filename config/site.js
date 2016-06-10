59e8c90c85c29227f6b0ff229cdab170f975ffa6
albertosouza <alberto.souza.99@gmail.com>
2016-06-10 07:19:44 -0300

    add some configurations with env vars

--- config/site.js
+++ config/site.js
@@ -1,14 +1,14 @@
 var path = require('path');
 module.exports = {
-  appName: 'We.js blog',
-  subtitle: 'A awsome we.js blog',
+  appName: process.env.APP_NAME || 'We.js blog',
+  subtitle: process.env.APP_DESCRIPTION || 'A awsome we.js blog',
 
   // default favicon, change in your project config/local.js
   favicon: path.resolve(__dirname, '..', 'files/public/favicon.png'),
   // logo public url path
-  appLogo: '/public/project/logo.jpg',
+  appLogo: process.env.APP_LOGO || '/public/project/logo.jpg',
 
   site: {
-    homeBg :'/public/project/home-bg.jpg'
+    homeBg : process.env.APP_HOMEBG || '/public/project/home-bg.jpg'
   }
 }
