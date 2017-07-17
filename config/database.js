5d2d07b59798d8a7fe887ec95fda0cc3eb51c145
Alberto Souza <alberto.souza.99@gmail.com>
2017-07-17 13:37:14 -0300

    improve dinamic configuration

--- config/database.js
+++ config/database.js
@@ -1,6 +1,10 @@
 module.exports.database = {
   prod: {
-    uri: process.env.DATABASE_URL
+    uri: process.env.DATABASE_URL,
+
+    database: process.env.DATABASE_NAME || 'test',
+    username: process.env.DATABASE_USERNAME || 'root',
+    password: process.env.DATABASE_PASSWORD
   },
   dev: {
 //    uri: process.env.DATABASE_URL,
@@ -9,9 +13,9 @@ module.exports.database = {
 //    protocol: 'postgres'
     uri: process.env.DATABASE_URL || null,
     dialect: 'mysql',
-    database: 'test',
-    username: 'root',
-    password: ''
+    database: process.env.DATABASE_NAME || 'test',
+    username: process.env.DATABASE_USERNAME || 'root',
+    password: process.env.DATABASE_PASSWORD
 
   },
   // localhost or dev env
